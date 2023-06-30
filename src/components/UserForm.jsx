e5e4067b374c6ddbbc2e1d999d426828dbf0ce6b
TCAM\GIS0938 <alfredo.aguilar@movistar.com.sv>
2023-06-30 13:40:24 -0600

    añadido manejo de for y validacion de campos vacios

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,20 +1,44 @@
 import { useState } from "react"
 
+const initialUserForm = {
+    username: '',
+    password: '',
+    email: '',
+}
 
 export function UserForm() {
 
+    //estado para controlar visibilidad de contraseña
     const [shown, setShown] = useState(false)
-
+    //estado para controlar inputs del form
+    const [userForm, setUserForm] =useState(initialUserForm)
+    //datos desestructurados del arreglo para el form 
+    const {username,email,password} = userForm
+    //funcion que controla visibilidad de contraseña
     const switchShown = () => {
         setShown(!shown)
     }
 
-    const onInputChange = ({target}) =>{
-        console.log(target.value)
+    //captura de datos en evento onChange, setea los valores computados de los inputs
+    const onInputChange = ({target}) =>{        
+        const {name, value} = target
+        setUserForm({
+            ...userForm,
+            [name]:value,            
+        })
     }   
+    //manejo del submit
+    const onFormSubmit = (event) =>{
+        event.preventDefault()
+        if(!username || !password || !email){
+            alert('completar los campos')
+        }
+        console.log(userForm)
+        setUserForm(initialUserForm)
+    } 
     return (
         <>
-            <form className="my-3 border border-1 border-secondary-subtle rounded">
+            <form className="my-3 border border-1 border-secondary-subtle rounded" onSubmit={onFormSubmit}>
                 <div className="row g-3 m-3 align-items-center">
                     <div className="col-auto w-25">
                         <label htmlFor="" className="col-form-label">Usuario: </label>
@@ -22,6 +46,7 @@ export function UserForm() {
                     <div className="col-auto w-75 ">
                         <input 
                             name="username" 
+                            value={username}
                             type="text" 
                             className="form-control" 
                             placeholder="Nombre de usuario"
@@ -36,6 +61,7 @@ export function UserForm() {
                     <div className="col-auto w-75 ">
                         <input 
                             name="email" 
+                            value={email}
                             type="text" 
                             className="form-control" 
                             placeholder="Correo"
@@ -49,7 +75,8 @@ export function UserForm() {
                     </div>
                     <div className="col-auto w-75 input-group" >
                         <input 
-                            name="password"                             
+                            name="password"          
+                            value={password}                   
                             type={shown ? 'text' : 'password'} 
                             className="form-control" 
                             placeholder="Contraseña"
@@ -66,7 +93,7 @@ export function UserForm() {
                 </div>
                 <div className="row g-3 m-3" >
                     <div className="col-auto">
-                        <button type="submit" className="btn btn-primary" >Enviar</button>
+                        <button type="submit" className="btn btn-primary" >Crear Usuario</button>
                     </div>
                 </div>
             </form>
