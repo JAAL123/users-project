fc4b1410770360ce66e627973611d9880ba881aa
TCAM\GIS0938 <alfredo.aguilar@movistar.com.sv>
2023-06-30 11:46:40 -0600

    añadida funcionalidad mostrar/ocultar contraseña

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react"
+
+
 export function UserForm() {
+
+    const [shown, setShown] = useState(false)
+
+    const switchShown = () => {
+        setShown(!shown)
+    }
+
+    const onInputChange = ({target}) =>{
+        console.log(target.value)
+    }   
     return (
         <>
             <form className="my-3 border border-1 border-secondary-subtle rounded">
@@ -7,7 +20,13 @@ export function UserForm() {
                         <label htmlFor="" className="col-form-label">Usuario: </label>
                     </div>
                     <div className="col-auto w-75 ">
-                        <input name="username" type="text" className="form-control" placeholder="Nombre de usuario"/>
+                        <input 
+                            name="username" 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="Nombre de usuario"
+                            onChange={onInputChange}
+                        />
                     </div>
                 </div>
                 <div className="row g-3 m-3 align-items-center">
@@ -15,7 +34,13 @@ export function UserForm() {
                         <label htmlFor="" className="col-form-label">Email: </label>
                     </div>
                     <div className="col-auto w-75 ">
-                        <input name="email" type="text" className="form-control" placeholder="Correo" />
+                        <input 
+                            name="email" 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="Correo"
+                            onChange={onInputChange} 
+                        />
                     </div>
                 </div>
                 <div className="row g-3 m-3 align-items-center ">
@@ -23,8 +48,20 @@ export function UserForm() {
                         <label htmlFor="" className="col-form-label">Contraseña: </label>
                     </div>
                     <div className="col-auto w-75 input-group" >
-                        <input name="password" type="Password" className="form-control" placeholder="Contraseña"/>
-                        <button className="btn btn-outline-secondary" type="button">Mostrar</button>
+                        <input 
+                            name="password"                             
+                            type={shown ? 'text' : 'password'} 
+                            className="form-control" 
+                            placeholder="Contraseña"
+                            onChange={onInputChange}
+                        />
+                        <button 
+                            className="btn btn-outline-secondary" 
+                            type="button"
+                            onClick={switchShown}
+                        >
+                            Mostrar
+                        </button>
                     </div>
                 </div>
                 <div className="row g-3 m-3" >
