5e48575e9249758175ae0a416a03f545363e3c4d
TCAM\GIS0938 <alfredo.aguilar@movistar.com.sv>
2023-07-03 09:32:45 -0600

    validacion de usuarios vacios

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -3,19 +3,19 @@ import './App.css'
 import { UserForm } from './components/UserForm';
 import { UsersList } from './components/UsersList';
 import { usersReducer } from './reducers/usersReducer';
-  //arreglo de usuarios inicial que se pasa a componente UserList
-  const initialUsers = [
-    {
-      id:1,
-      username:'pepe',
-      password: '12345',
-      email: 'pepe@correo.com'
-    },
-  ]
-  const initialUserForm = {
-    username: '',
-    password: '',
-    email: '',
+//arreglo de usuarios inicial que se pasa a componente UserList
+const initialUsers = [
+  {
+    id: 1,
+    username: 'pepe',
+    password: '12345',
+    email: 'pepe@correo.com'
+  },
+]
+const initialUserForm = {
+  username: '',
+  password: '',
+  email: '',
 }
 
 
@@ -25,7 +25,7 @@ function UsersApp() {
   const [users, dispatch] = useReducer(usersReducer, initialUsers)
 
   const handlerAddUser = (user) => {
-   
+
     dispatch({
       type: 'addUser',
       payload: user,
@@ -35,30 +35,35 @@ function UsersApp() {
   //funcion que se manda al reducer y recibe como parametro un id
   const handlerRemoveUser = (id) => {
     dispatch({
-      type:'removeUser',
-      payload: id, 
+      type: 'removeUser',
+      payload: id,
     })
   }
 
 
-//vista de la tabla de ususarios y formulario de usuarios
+  //vista de la tabla de ususarios y formulario de usuarios
   return (
     <>
       <div className="container my-4">
         <h1>App de ususarios</h1>
         <div className='row mt-2'>
-        <div className='col'>
-          <UserForm
-            handlerAddUser={handlerAddUser}  
-            initialUserForm={initialUserForm}          
-          />
-        </div>
-        <div className='col'>
-          <UsersList 
-            users={users}
-            handlerRemoveUser={handlerRemoveUser}
-          />
-        </div>
+          <div className='col'>
+            <UserForm
+              handlerAddUser={handlerAddUser}
+              initialUserForm={initialUserForm}
+            />
+          </div>
+          <div className='col'>
+            {
+              users.length === 0 ?
+                <div className='alert alert-warning my-3 text-center'>No hay usuarios en el sistema</div>
+                :
+                <UsersList
+                  users={users}
+                  handlerRemoveUser={handlerRemoveUser}
+                />
+            }
+          </div>
         </div>
       </div>
     </>
