ca74b07b47632f6271f9797e384a93c43f159dd1
TCAM\GIS0938 <alfredo.aguilar@movistar.com.sv>
2023-07-04 09:34:48 -0600

    estructura para realizar login

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,61 +1,13 @@
 import './Styles.css'
 import { LoginPage } from './auth/pages/LoginPage';
-import { USerModalForm } from './components/UserModalForm';
-import { UsersList } from './components/UsersList';
-import { useUsers } from './hooks/useUsers';
-
+import { UserPage } from './pages/UserPage';
 
 function UsersApp() {
 
-  const {
-    users,
-    userSelected,
-    initialUserForm,
-    formIsVisible,
-    handlerAddUser,
-    handlerRemoveUser,
-    handlerSelectedUser,
-    handlerOpenForm,
-    handlerCloseForm,
-  } = useUsers()
-
-  //vista de la tabla de ususarios y formulario de usuarios
-  return (
+  return(
     <>
-      {
-        !formIsVisible ||
-        <USerModalForm
-          handlerAddUser={handlerAddUser}
-          userSelected={userSelected}
-          initialUserForm={initialUserForm}
-          handlerCloseForm={handlerCloseForm}
-        />
-      }
-      <div className="container-fluid my-4">
-        <h1>App de Usuarios</h1>
-        <div className='row mt-2'>
-          <div className='col-lg'>
-            {
-              formIsVisible || <button
-                className='btn btn-primary my-2'
-                onClick={handlerOpenForm}
-              >
-                Nuevo usuario
-              </button>
-            }
-            {
-              users.length === 0 ?
-                <div className='alert alert-warning my-3 text-center'>No hay usuarios en el sistema</div>
-                :
-                <UsersList
-                  users={users}
-                  handlerRemoveUser={handlerRemoveUser}
-                  handlerSelectedUser={handlerSelectedUser}
-                />
-            }
-          </div>
-        </div>
-      </div>
+      <LoginPage/>
+      {/* <UserPage/> */}
     </>
   )
 }
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,58 @@
+import '../Styles.css'
+import { USerModalForm } from "../components/UserModalForm"
+import { UsersList } from "../components/UsersList"
+import { useUsers } from "../hooks/useUsers"
+
+export function UserPage() {
+    const {
+        users,
+        userSelected,
+        initialUserForm,
+        formIsVisible,
+        handlerAddUser,
+        handlerRemoveUser,
+        handlerSelectedUser,
+        handlerOpenForm,
+        handlerCloseForm,
+      } = useUsers()
+    
+      //vista de la tabla de ususarios y formulario de usuarios
+      return (
+        <>
+          {
+            !formIsVisible ||
+            <USerModalForm
+              handlerAddUser={handlerAddUser}
+              userSelected={userSelected}
+              initialUserForm={initialUserForm}
+              handlerCloseForm={handlerCloseForm}
+            />
+          }
+          <div className="container-fluid my-4">
+            <h1>App de Usuarios</h1>
+            <div className='row mt-2'>
+              <div className='col-lg'>
+                {
+                  formIsVisible || <button
+                    className='btn btn-primary my-2'
+                    onClick={handlerOpenForm}
+                  >
+                    Nuevo usuario
+                  </button>
+                }
+                {
+                  users.length === 0 ?
+                    <div className='alert alert-warning my-3 text-center'>No hay usuarios en el sistema</div>
+                    :
+                    <UsersList
+                      users={users}
+                      handlerRemoveUser={handlerRemoveUser}
+                      handlerSelectedUser={handlerSelectedUser}
+                    />
+                }
+              </div>
+            </div>
+          </div>
+        </>
+      )
+}
